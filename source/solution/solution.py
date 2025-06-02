dfcedc9c9edc40837636561fdaa986921af4ea47
MJRI <stpe95@gmail.com>
2025-06-02 23:39:53 +0900

    update solution

--- source/solution/solution.py
+++ source/solution/solution.py
@@ -1,25 +1,40 @@
 import json
+from pathlib import Path
 
 
 class Solution:
     def __init__(self):
         # solution information
-        self.save_path = None
-        self.solution_name = None
+        self.root = Path()
+        self.name = Path()
+        self.json_name = Path()
 
         # task information
         self.task = None
 
     def to_dict(self):
-        return self.__dict__
+        result = {}
+        for k, v in self.__dict__.items():
+            if isinstance(v, Path):
+                result[k] = str(v)
+            else:
+                result[k] = v
+        return result
 
     @classmethod
     def from_dict(cls, data):
         obj = cls()
-        obj.__dict__.update(data)
+        # Path 객체로 변환
+        obj.root = Path(data.get("root", ""))
+        obj.name = Path(data.get("name", ""))
+        obj.json_name = Path(data.get("json_name", ""))
+        obj.task = data.get("task", None)
+        # 다른 멤버가 있다면 여기에 추가
         return obj
 
-    def save_json(self, file_path):
+    def save_json(self, file_path=None):
+        if file_path is None:
+            file_path = self.root / self.name / self.json_name
         with open(file_path, "w", encoding="utf-8") as f:
             json.dump(self.to_dict(), f, ensure_ascii=False, indent=4)
 
