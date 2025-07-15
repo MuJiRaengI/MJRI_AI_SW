2b75e6af156acb2f0b475b308c9e7ce6f753fca6
MJRI <stpe95@gmail.com>
2025-07-15 23:01:40 +0900

    add contributors

--- ui/designer/ui_contributors.py
+++ ui/designer/ui_contributors.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'contributorsBpFgef.ui'
+## Form generated from reading UI file 'contributorsLLMapy.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.1
 ##
@@ -27,22 +27,53 @@ class Ui_Contributors(object):
         self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
+        self.label_4 = QLabel(Contributors)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.gridLayout.addWidget(self.label_4, 3, 4, 1, 1)
+
+        self.line_3 = QFrame(Contributors)
+        self.line_3.setObjectName(u"line_3")
+        self.line_3.setFrameShape(QFrame.Shape.VLine)
+        self.line_3.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.gridLayout.addWidget(self.line_3, 2, 3, 4, 1)
+
+        self.lbl_git = QLabel(Contributors)
+        self.lbl_git.setObjectName(u"lbl_git")
+        font = QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        self.lbl_git.setFont(font)
+        self.lbl_git.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.gridLayout.addWidget(self.lbl_git, 2, 4, 1, 1)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout.addItem(self.verticalSpacer, 5, 0, 1, 1)
+
+        self.line_2 = QFrame(Contributors)
+        self.line_2.setObjectName(u"line_2")
+        self.line_2.setFrameShape(QFrame.Shape.VLine)
+        self.line_2.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.gridLayout.addWidget(self.line_2, 2, 1, 4, 1)
+
         self.lbl_title = QLabel(Contributors)
         self.lbl_title.setObjectName(u"lbl_title")
-        font = QFont()
-        font.setPointSize(15)
-        font.setBold(True)
-        self.lbl_title.setFont(font)
+        font1 = QFont()
+        font1.setPointSize(15)
+        font1.setBold(True)
+        self.lbl_title.setFont(font1)
         self.lbl_title.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.gridLayout.addWidget(self.lbl_title, 0, 0, 1, 5)
 
         self.lbl_chzzk = QLabel(Contributors)
         self.lbl_chzzk.setObjectName(u"lbl_chzzk")
-        font1 = QFont()
-        font1.setPointSize(12)
-        font1.setBold(True)
-        self.lbl_chzzk.setFont(font1)
+        self.lbl_chzzk.setFont(font)
         self.lbl_chzzk.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.gridLayout.addWidget(self.lbl_chzzk, 2, 0, 1, 1)
@@ -50,28 +81,10 @@ class Ui_Contributors(object):
         self.lbl_youtube = QLabel(Contributors)
         self.lbl_youtube.setObjectName(u"lbl_youtube")
         self.lbl_youtube.setMaximumSize(QSize(16777215, 16777215))
-        self.lbl_youtube.setFont(font1)
+        self.lbl_youtube.setFont(font)
         self.lbl_youtube.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.gridLayout.addWidget(self.lbl_youtube, 2, 2, 1, 1)
-
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-
-        self.gridLayout.addItem(self.verticalSpacer, 4, 0, 1, 1)
-
-        self.line_3 = QFrame(Contributors)
-        self.line_3.setObjectName(u"line_3")
-        self.line_3.setFrameShape(QFrame.Shape.VLine)
-        self.line_3.setFrameShadow(QFrame.Shadow.Sunken)
-
-        self.gridLayout.addWidget(self.line_3, 2, 3, 3, 1)
-
-        self.line_2 = QFrame(Contributors)
-        self.line_2.setObjectName(u"line_2")
-        self.line_2.setFrameShape(QFrame.Shape.VLine)
-        self.line_2.setFrameShadow(QFrame.Shadow.Sunken)
-
-        self.gridLayout.addWidget(self.line_2, 2, 1, 3, 1)
 
         self.line = QFrame(Contributors)
         self.line.setObjectName(u"line")
@@ -80,18 +93,11 @@ class Ui_Contributors(object):
 
         self.gridLayout.addWidget(self.line, 1, 0, 1, 5)
 
-        self.lbl_git = QLabel(Contributors)
-        self.lbl_git.setObjectName(u"lbl_git")
-        self.lbl_git.setFont(font1)
-        self.lbl_git.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label = QLabel(Contributors)
+        self.label.setObjectName(u"label")
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.gridLayout.addWidget(self.lbl_git, 2, 4, 1, 1)
-
-        self.label_4 = QLabel(Contributors)
-        self.label_4.setObjectName(u"label_4")
-        self.label_4.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        self.gridLayout.addWidget(self.label_4, 3, 4, 1, 1)
+        self.gridLayout.addWidget(self.label, 4, 4, 1, 1)
 
 
         self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
@@ -104,10 +110,11 @@ class Ui_Contributors(object):
 
     def retranslateUi(self, Contributors):
         Contributors.setWindowTitle(QCoreApplication.translate("Contributors", u"Contributors", None))
+        self.label_4.setText(QCoreApplication.translate("Contributors", u"Tyndall log", None))
+        self.lbl_git.setText(QCoreApplication.translate("Contributors", u"[ Git ]", None))
         self.lbl_title.setText(QCoreApplication.translate("Contributors", u"Contributors", None))
         self.lbl_chzzk.setText(QCoreApplication.translate("Contributors", u"[ Chzzk ]", None))
         self.lbl_youtube.setText(QCoreApplication.translate("Contributors", u"[ Youtube ]", None))
-        self.lbl_git.setText(QCoreApplication.translate("Contributors", u"[ Git ]", None))
-        self.label_4.setText(QCoreApplication.translate("Contributors", u"Tyndall log", None))
+        self.label.setText(QCoreApplication.translate("Contributors", u"great0108", None))
     # retranslateUi
 
